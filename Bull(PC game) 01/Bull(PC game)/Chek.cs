897162b0bbd369b48016a82c08e3f304a65b2438
KindlyMonster <m0nst3rliveeee@gmail.com>
2018-11-12 23:59:51 +0400

    2ая часть 1 домашки

---
+++ Bull(PC game) 01/Bull(PC game)/Chek.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bull_PC_game_
+{
+    class Chek
+    {
+        private static int[] InNum = new int[4];
+
+        public void GetInNum()
+        {
+            Console.WriteLine($"PC WIN NUMBER");
+            for (int i=0; i<4;i++)
+            {
+                Console.Write($"{InNum[i]}");
+            }
+        }
+
+        public void Cheking(int number)
+        {
+            int k = 0, k1 = 0, bulls=0;
+
+            int[] ChNum = new int[4];
+            
+            for (var i = 3; i != -1; i--)
+            {
+                 ChNum[i] = number % 10;
+                 number = number / 10;
+            }
+            int dig = 0;
+            while (bulls != 4)
+            {
+                if (InNum[k] != ChNum[k])
+                {
+                    k1++;
+                    InNum[k] = dig;
+                    dig++;
+                    //Console.WriteLine($"{InNum[k]}");
+                }
+                else
+                {
+                    if (k < 4)
+                    {
+                        //Console.WriteLine($"----{k}----");
+                        k++;
+                        dig = 0;
+                        bulls++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Bull(PC game) 01/Bull(PC game)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bull_PC_game_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new Chek();
+            int number;
+
+            Console.Write("Загадай число : ");
+            number = Convert.ToInt32(Console.ReadLine());
+            num.Cheking(number);
+            num.GetInNum();
+            Console.ReadKey();
+        }
+    }
+}
